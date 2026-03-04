c8addde3d52e73a211643b5dce07a38fc370a488
Soniya Joshi <1nt23cs238.soniya@nmit.ac.in>
2026-03-05 01:43:01 +0530

    Create smart_light_system.ino

---
+++ code/code/smart_light_system.ino
@@ -0,0 +1,14 @@
+// IoT Smart Campus Monitoring System
+// Motion-based Smart Lighting using NodeMCU (ESP8266)
+
+// This file will contain the main Arduino code for the project.
+// The system detects motion using a PIR sensor and automatically
+// controls lighting through a relay module.
+
+void setup() {
+  // Initialization code will be added here
+}
+
+void loop() {
+  // Motion detection and light control logic will be added here
+}
